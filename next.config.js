ff40c65ee318e86c626f63d07db59e68486dd2e2
Saull Brandão <saull@outlook.com>
2022-03-05 13:58:15 -0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.tsx?$/,
+      include: [options.dir],
+      use: [
+        'next-swc-loader',
+        {
+          loader: '@svgr/webpack',
+          options: { babel: false },
+        },
+      ],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
