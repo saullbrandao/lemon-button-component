aafc8048baf2acbfd8ed740c96b21ec7c0f48608
Saull Brandão <saull@outlook.com>
2022-03-07 08:44:20 -0300

    chore: remove unused import

--- src/components/LemonButton/types.ts
+++ src/components/LemonButton/types.ts
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, MouseEventHandler, ReactNode } from 'react'
+import type { ButtonHTMLAttributes, ReactNode } from 'react'
 
 interface CommonButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color: 'primary' | 'secondary'
